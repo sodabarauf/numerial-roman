5acceace4342f1aac40ad10f2de03bef6e546ca2
sodabarauf <sodabarauf4@gmail.com>
2024-06-27 08:21:26 +0430

    fixed error

--- javascript.js
+++ javascript.js
@@ -1,8 +1,20 @@
 function convertToRoman(num) {
   const map = {
-    M: 1000, CM: 900, D: 500, CD: 400, C: 100, XC: 90, L: 50, 
-    XL: 40, X: 10, IX: 9, V: 5, IV: 4, I: 1,
+    M: 1000, 
+    CM: 900, 
+    D: 500, 
+    CD: 400, 
+    C: 100, 
+    XC: 90, 
+    L: 50, 
+    XL: 40, 
+    X: 10, 
+    IX: 9, 
+    V: 5, 
+    IV: 4, 
+    I: 1,
   };
+
   let roman = '';                    
   for (const i in map) {                   
     while (num >= map[i]) {            
